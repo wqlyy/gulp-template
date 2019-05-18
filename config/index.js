de2418b0077175c76cda15b2dcd47762efe13c34
ruiaofeng <raf@ruiaofengdeMacBook-Pro.local>
2019-05-19 01:51:08 +0800

    搭建gulp自动化工具

---
+++ config/index.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const server = require('./server')
+
+function resolveDev(dir) {
+  return path.join(__dirname, '../src/', dir)
+}
+
+function resolveBuild(dir) {
+  return path.join(__dirname, '../dist/', dir)
+}
+
+module.exports = {
+  dev: {
+    static: './static/**/*',
+    html:  [resolveDev('/**/*.html'), '!./src/include/**/*'],
+    allhtml: resolveDev('/**/*.html'),
+    styles: [resolveDev('static/styles/*.{less,scss,css}'),'!./src/static/styles/_bootstrap/*.{less,scss,css}'],
+    allStyles:resolveDev('/**/*.{less,scss,css}'),
+    script: resolveDev('static/js/**/*.js'),
+    images: resolveDev('static/images/**/*.{png,jpg,gif,svg}'),
+  },
+
+  build: {
+    static: resolveBuild('static'),
+    html: resolveBuild(''),
+    styles: resolveBuild('static/css'),
+    script: resolveBuild('static/js'),
+    images: resolveBuild('static/images'),
+  },
+
+  zip: {
+    name: 'gulpProject.zip',
+    path: resolveBuild('**/*'),
+    dest: path.join(__dirname, '../')
+  },
+
+  server,
+  useEslint: false,
+  useWebpack: false,
+  productionZip: false
+}
